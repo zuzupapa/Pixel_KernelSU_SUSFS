c9648105bf99735661b0c601a863d98ddb53c358
Egor <knowwhatdontknow@gmail.com>
2026-05-16 00:56:39 +0300

    fix: Python post-patch for init.c hunk #3 + skip broken sed pre-patch

---
+++ .github/actions/susfs/fix_init_hunk3.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Fix init.c hunk #3 reject from ksun SUSFS combined patch.
+
+The patch was built against an older KSU-Next without allow_shell.
+Newer KSU-Next (f08381731+) added allow_shell between ksu_late_loaded
+and kernelsu_init(), breaking patch context matching.
+
+This removes what hunk #3 would have removed:
+  - bool ksu_late_loaded;
+  - #if defined(__x86_64__) ... #endif
+  - #ifdef MODULE ... ksu_late_loaded ... #endif
+"""
+import re, sys
+
+path = sys.argv[1] if len(sys.argv) > 1 else 'kernel/core/init.c'
+
+with open(path) as f:
+    c = f.read()
+
+c = re.sub(r'bool ksu_late_loaded;\s*\n', '', c)
+c = re.sub(r'#if defined\(__x86_64__\).*?#endif\s*\n', '', c, flags=re.DOTALL)
+c = re.sub(r'#ifdef MODULE\s*\n\s*ksu_late_loaded.*?#endif\s*\n', '', c, flags=re.DOTALL)
+
+with open(path, 'w') as f:
+    f.write(c)
+
+print(f"Fixed {path}: removed ksu_late_loaded, x86_64 check, late_loaded init")
